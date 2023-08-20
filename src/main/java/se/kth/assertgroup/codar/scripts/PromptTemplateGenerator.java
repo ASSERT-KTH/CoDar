890db0260fcc8a2ad81beacf0269b8202db391a7
Khashayar Etemadi <khaes@kth.se>
2023-08-20 19:27:11 +0200

    chore: all prompts added

--- src/main/java/se/kth/assertgroup/codar/scripts/PromptTemplateGenerator.java
+++ src/main/java/se/kth/assertgroup/codar/scripts/PromptTemplateGenerator.java
@@ -9,12 +9,17 @@ import se.kth.assertgroup.codar.Constants;
 import se.kth.assertgroup.codar.sonar.SonarDocumentInfo;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PromptTemplateGenerator {
-    public void generatePromptTemplates(String rule) throws IOException {
+    public void generatePromptTemplates(String rule) throws IOException, URISyntaxException {
         String url = Constants.SONAR_DOC_URL_TEMPLATE.replace(Constants.SONAR_DOC_URL_RULE_PLACEHOLDER,
                 rule.substring(1)); // remove S at the beginning of the rule
         Document doc = Jsoup.connect(url).get();
@@ -28,21 +33,23 @@ public class PromptTemplateGenerator {
 
         docInfo.moveToUnifiedFullDescriptionFormat();
 
-        if (!areExamplesCorrectlyOrdered(doc, docInfo.getFullDescription()))
-            throw new IllegalArgumentException("Documentation doesn't have correct examples");
+        docInfo.updateExampleIndices();
 
-        docInfo.moveToUnifiedFullDescriptionFormat();
+        if (!docInfo.includesProperExample())
+            throw new IllegalArgumentException("Documentation doesn't have correct examples");
 
         docInfo.cleanDescriptions();
 
-        addExamplesToDocInfo(doc, docInfo);
+        docInfo.addExamplesToDocInfo(doc);
+
+        docInfo.removeCompliantCommentsFromDescription();
 
         docInfo.cleanData();
 
         printPromptTemplates(docInfo);
     }
 
-    private void printPromptTemplates(SonarDocumentInfo docInfo) throws IOException {
+    private void printPromptTemplates(SonarDocumentInfo docInfo) throws IOException, URISyntaxException {
         printZeroShotPromptTemplate(docInfo);
         printExamplePromptTemplate(docInfo);
         printTitlePromptTemplate(docInfo);
@@ -55,39 +62,39 @@ public class PromptTemplateGenerator {
         String prompt = docInfo.getTitle() + System.lineSeparator()
                 + docInfo.getFullDescription() + System.lineSeparator() + Constants.PROMPT_ENDING;
 
-        FileUtils.write(new File(Constants.FULL_DESCRIPTION_PROMPTS_DIR + docInfo.getRule()),
-                prompt, "UTF-8");
+        writeToResource(Constants.FULL_DESCRIPTION_PROMPTS_DIR + docInfo.getRule(), prompt);
     }
 
     private void printTitleDescriptionExamplePromptTemplate(SonarDocumentInfo docInfo) throws IOException {
         String prompt = docInfo.getTitle() + System.lineSeparator() + docInfo.getDescriptionWithNoEx()
                 + System.lineSeparator() + getPromptExampleSection(docInfo);
 
-        FileUtils.write(new File(Constants.TITLE_DESCRIPTION_Example_PROMPTS_DIR + docInfo.getRule()),
-                prompt, "UTF-8");
+        writeToResource(Constants.TITLE_DESCRIPTION_Example_PROMPTS_DIR + docInfo.getRule(), prompt);
     }
 
     private void printTitleDescriptionPromptTemplate(SonarDocumentInfo docInfo) throws IOException {
         String prompt = docInfo.getTitle() + System.lineSeparator() + docInfo.getDescriptionWithNoEx()
                 + System.lineSeparator() + Constants.PROMPT_ENDING;
 
-        FileUtils.write(new File(Constants.TITLE_DESCRIPTION_PROMPTS_DIR + docInfo.getRule()),
-                prompt, "UTF-8");
+        writeToResource(Constants.TITLE_DESCRIPTION_PROMPTS_DIR + docInfo.getRule(), prompt);
     }
 
     private void printTitlePromptTemplate(SonarDocumentInfo docInfo) throws IOException {
         String prompt = docInfo.getTitle() + System.lineSeparator()
                 + Constants.PROMPT_ENDING;
 
-        FileUtils.write(new File(Constants.TITLE_PROMPTS_DIR + docInfo.getRule()),
-                prompt, "UTF-8");
+        writeToResource(Constants.TITLE_PROMPTS_DIR + docInfo.getRule(), prompt);
     }
 
     private void printExamplePromptTemplate(SonarDocumentInfo docInfo) throws IOException {
         String prompt = getPromptExampleSection(docInfo);
 
-        FileUtils.write(new File(Constants.EXAMPLE_PROMPTS_DIR + docInfo.getRule()),
-                prompt, "UTF-8");
+        writeToResource(Constants.EXAMPLE_PROMPTS_DIR + docInfo.getRule(), prompt);
+    }
+
+    private void writeToResource(String rscPath, String text) throws IOException{
+        FileUtils.write(new File("/home/khaes/phd/projects/sonar-codex/src/main/resources/" + rscPath), text,
+                "UTF-8");
     }
 
     private String getPromptExampleSection(SonarDocumentInfo docInfo) {
@@ -98,48 +105,14 @@ public class PromptTemplateGenerator {
                 + Constants.PROMPT_ENDING;
     }
 
-    private void printZeroShotPromptTemplate(SonarDocumentInfo docInfo) throws IOException {
-        FileUtils.write(new File(Constants.ZERO_SHOT_PROMPTS_DIR + docInfo.getRule()),
-                Constants.PROMPT_ENDING, "UTF-8");
-    }
-
-    private void addExamplesToDocInfo(Document jsoupDoc, SonarDocumentInfo docInfo) {
-        Elements snippets = jsoupDoc.getElementsByTag("pre");
-        for (int i = 0; i < snippets.size(); i += 2) {
-            SonarDocumentInfo.ViolationExample ex = new SonarDocumentInfo.ViolationExample();
-            ex.setNonCompliant(snippets.get(i).text());
-            ex.setCompliant(snippets.get(i + 1).text());
-            docInfo.getExamples().add(ex);
-        }
+    private void printZeroShotPromptTemplate(SonarDocumentInfo docInfo) throws IOException, URISyntaxException {
+        writeToResource(Constants.ZERO_SHOT_PROMPTS_DIR + docInfo.getRule(), Constants.PROMPT_ENDING);
     }
 
     private String extractFullDescription(Document doc){
         Element descriptionSection = doc.getElementsByTag("section").get(2);
         String description = descriptionSection.text();
         return description;
-    }
-
-    private boolean areExamplesCorrectlyOrdered(Document doc, String description) {
-        List<Integer> nonCompliantIndices = findWord(description, "Noncompliant Code Example "),
-                compliantIndices = findWord(description, "Compliant Solution ");
-
-        if (nonCompliantIndices.size() != compliantIndices.size())
-            return false;
-
-        for (int i = 1; i < nonCompliantIndices.size(); i++) {
-            if (nonCompliantIndices.get(i) > compliantIndices.get(i)
-                    || nonCompliantIndices.get(i) < compliantIndices.get(i - 1))
-                return false;
-        }
-
-        if (nonCompliantIndices.get(0) > compliantIndices.get(0))
-            return false;
-
-        if (doc.getElementsByTag("pre").size() != nonCompliantIndices.size() * 2
-                || nonCompliantIndices.size() != 1) // only consider rules with one example
-            return false;
-
-        return true;
     }
 
     private String getRuleDocTitle(Document doc) throws IOException {
@@ -164,21 +137,21 @@ public class PromptTemplateGenerator {
         return indexes;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         PromptTemplateGenerator ptg = new PromptTemplateGenerator();
 
-//        ptg.generatePromptTemplates("S5042");
+        ptg.generatePromptTemplates("S1217");
 
-        List<String> rules =
-                FileUtils.readLines(new File("/home/khaes/tmp/rules.txt"), "UTF-8");
-
-        rules.forEach(r -> {
-            try {
-                System.out.println("Check for: " + r);
-                ptg.generatePromptTemplates(r);
-            } catch (Exception e) {
-                System.out.println("Didn't work for " + r);
-            }
-        });
+//        List<String> rules =
+//                FileUtils.readLines(new File("/home/khaes/tmp/rules.txt"), "UTF-8");
+//
+//        rules.forEach(r -> {
+//            try {
+//                System.out.println("Check for: " + r);
+//                ptg.generatePromptTemplates(r);
+//            } catch (Exception e) {
+//                System.out.println("Didn't work for " + r);
+//            }
+//        });
     }
 }
--- src/main/java/se/kth/assertgroup/codar/sonar/SonarDocumentInfo.java
+++ src/main/java/se/kth/assertgroup/codar/sonar/SonarDocumentInfo.java
@@ -1,5 +1,9 @@
 package se.kth.assertgroup.codar.sonar;
 
+import org.jetbrains.annotations.NotNull;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import se.kth.assertgroup.codar.Constants;
 
 import java.util.ArrayList;
@@ -10,41 +14,54 @@ import java.util.stream.Collectors;
 public class SonarDocumentInfo {
     private String rule, title, fullDescription;
     private List<ViolationExample> examples;
+    private List<Integer> nonCompliantIndices, compliantIndices;
 
     public SonarDocumentInfo(){
         this.examples = new ArrayList<>();
     }
 
     public void cleanDescriptions() {
-        boolean isCode = false, isCompliant = false;
 
         // Replacing example labels
         fullDescription = fullDescription.replaceAll("Noncompliant Code Example ",
                 System.lineSeparator() + "Noncompliant Code Example" + System.lineSeparator())
                 .replaceAll("Compliant Solution ",
+                        System.lineSeparator() + "Compliant Solution" + System.lineSeparator())
+                .replaceAll("Noncompliant code example ",
+                        System.lineSeparator() + "Noncompliant Code Example" + System.lineSeparator())
+                .replaceAll("Compliant solution ",
                         System.lineSeparator() + "Compliant Solution" + System.lineSeparator());
-
-        String[] desLines = fullDescription.split(System.lineSeparator());
-
-        for (int i = 0; i < desLines.length; i++) {
-            if (desLines[i].contains("Noncompliant Code Example")) {
-                isCode = true;
-                isCompliant = false;
-            }
-            if (desLines[i].contains("Compliant Solution")) {
-                isCompliant = true;
-            }
-
-            if (isCode && !isCompliant && desLines[i].contains("//") && desLines[i].contains("Noncompliant")
-                    && desLines[i].indexOf("//") < desLines[i].indexOf("Noncompliant"))
-                desLines[i] = desLines[i].substring(0, desLines[i].indexOf("//"));
-        }
-
-        Arrays.asList(desLines).stream().collect(Collectors.joining(System.lineSeparator()));
 
         // Replacing example labels
         fullDescription = fullDescription.replaceAll("Noncompliant Code Example", Constants.PROMPT_NONCOMPLIANT_HEADER)
                 .replaceAll("Compliant Solution", Constants.PROMPT_COMPLIANT_HEADER);
+    }
+
+    public void removeCompliantCommentsFromDescription(){
+        fullDescription = removeCompliantComment(fullDescription);
+    }
+
+    public void addExamplesToDocInfo(Document jsoupDoc) {
+        Elements snippets = jsoupDoc.getElementsByTag("pre");
+        Element firstSnippetAfterNonCompliant = null, firstSnippetAfterCompliant = null;
+
+        for (int i = 0; i < snippets.size(); i++) {
+            String txt = snippets.get(i).text();
+            if(fullDescription.indexOf(txt) >= nonCompliantIndices.get(0)
+                    && (firstSnippetAfterNonCompliant == null
+                    || fullDescription.indexOf(txt) <= fullDescription.indexOf(firstSnippetAfterNonCompliant.text())))
+                firstSnippetAfterNonCompliant = snippets.get(i);
+
+            if(fullDescription.indexOf(txt) >= compliantIndices.get(0)
+                    && (firstSnippetAfterCompliant == null
+                    || fullDescription.indexOf(txt) <= fullDescription.indexOf(firstSnippetAfterCompliant.text())))
+                firstSnippetAfterCompliant = snippets.get(i);
+        }
+
+        SonarDocumentInfo.ViolationExample ex = new SonarDocumentInfo.ViolationExample();
+        ex.setNonCompliant(removeCompliantComment(firstSnippetAfterNonCompliant.text()));
+        ex.setCompliant(removeCompliantComment(firstSnippetAfterCompliant.text()));
+        examples.add(ex);
     }
 
     public List<ViolationExample> getExamples() {
@@ -66,20 +83,41 @@ public class SonarDocumentInfo {
     private void cleanExamples(){
         for(ViolationExample ex : examples){
             String rawCompliant = ex.getCompliant();
-            String[] lines = rawCompliant.split(System.lineSeparator());
-            for (int i = 0; i < lines.length; i++) {
-                if(lines[i].contains("//") && lines[i].contains("Compliant")
-                        && lines[i].indexOf("//") < lines[i].indexOf("Compliant"))
-                lines[i] = lines[i].substring(0, lines[i].indexOf("//"));
-            }
-            rawCompliant = Arrays.asList(lines).stream().collect(Collectors.joining(System.lineSeparator()));
-            ex.setCompliant(rawCompliant);
+            ex.setCompliant(removeCompliantComment(rawCompliant));
         }
+    }
+
+    @NotNull
+    private static String removeCompliantComment(String rawCompliant) {
+        String[] lines = rawCompliant.split(System.lineSeparator());
+        for (int i = 0; i < lines.length; i++) {
+            if(lines[i].contains("//") && lines[i].contains("Compliant")
+                    && lines[i].indexOf("//") < lines[i].indexOf("Compliant"))
+            lines[i] = lines[i].substring(0, lines[i].indexOf("//"));
+        }
+        rawCompliant = Arrays.asList(lines).stream().collect(Collectors.joining(System.lineSeparator()));
+        return rawCompliant;
     }
 
     public void moveToUnifiedFullDescriptionFormat() {
         this.fullDescription = this.fullDescription.replaceAll("Sensitive Code Example",
                 "Noncompliant Code Example");
+    }
+
+    public List<Integer> getNonCompliantIndices() {
+        return nonCompliantIndices;
+    }
+
+    public void setNonCompliantIndices(List<Integer> nonCompliantIndices) {
+        this.nonCompliantIndices = nonCompliantIndices;
+    }
+
+    public List<Integer> getCompliantIndices() {
+        return compliantIndices;
+    }
+
+    public void setCompliantIndices(List<Integer> compliantIndices) {
+        this.compliantIndices = compliantIndices;
     }
 
     public static class ViolationExample {
@@ -120,6 +158,31 @@ public class SonarDocumentInfo {
 
     public String getFullDescription() {
         return fullDescription;
+    }
+
+    public void updateExampleIndices() {
+        nonCompliantIndices = findWord(fullDescription, "Noncompliant Code Example ");
+        compliantIndices = findWord(fullDescription, "Compliant Solution ");
+    }
+
+    public boolean includesProperExample() {
+        return nonCompliantIndices.size() > 0 && compliantIndices.size() > 0;
+    }
+
+    private List<Integer> findWord(String textString, String word) {
+        List<Integer> indexes = new ArrayList<Integer>();
+        String lowerCaseTextString = textString.toLowerCase();
+        String lowerCaseWord = word.toLowerCase();
+
+        int index = 0;
+        while (index != -1) {
+            index = lowerCaseTextString.indexOf(lowerCaseWord, index);
+            if (index != -1) {
+                indexes.add(index);
+                index++;
+            }
+        }
+        return indexes;
     }
 
     public void setFullDescription(String fullDescription) {
